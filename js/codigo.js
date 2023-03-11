2e58f3d2b498fc22726979abb3f109a658ce6e30
josefina2162 <joseezappettini@gmail.com>
2023-03-11 13:15:14 -0300

    SASS

---
+++ js/codigo.js
@@ -0,0 +1,74 @@
+function mostrarD(){
+    document.getElementById('carruselDulce').style.display = 'block';
+}
+
+function mostrarR(){
+    document.getElementById('carruselRegalos').style.display = 'block';
+}
+
+function mostrarS(){
+    document.getElementById('carruselSalado').style.display = 'block';
+}
+
+
+function ocultarS(){
+    document.getElementById('carruselSalado').style.display = 'none';
+}
+
+function ocultarR(){
+    document.getElementById('carruselRegalos').style.display = 'none';
+}
+
+function ocultarD(){
+    document.getElementById('carruselDulce').style.display = 'none';
+}
+
+function mostrarDropD(){
+    document.getElementById('dropDulce').style.display = 'block';
+}
+
+function mostrarDropR(){
+    document.getElementById('dropRegalos').style.display = 'block';
+}
+
+function mostrarDropS(){
+    document.getElementById('dropSalado').style.display = 'block';
+}
+
+function ocultarDropS(){
+    document.getElementById('dropSalado').style.display = 'none';
+}
+
+function ocultarDropD(){
+    document.getElementById('dropDulce').style.display = 'none';
+}
+
+function ocultarDropR(){
+    document.getElementById('dropRegalos').style.display = 'none';
+}
+
+
+
+function ocultarDeskD(){
+    document.getElementById('filaD').style.display = 'none';
+}
+
+function ocultarDeskR(){
+    document.getElementById('filaR').style.display = 'none';
+}
+
+function ocultarDeskS(){
+    document.getElementById('filaS').style.display = 'none';
+}
+
+function mostrarDeskD(){
+    document.getElementById('filaD').style.display = 'block';
+}
+
+function mostrarDeskR(){
+    document.getElementById('filaR').style.display = 'block';
+}
+
+function mostrarDeskS(){
+    document.getElementById('filaS').style.display = 'block';
+}
